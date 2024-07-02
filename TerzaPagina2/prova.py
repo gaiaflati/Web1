b7b40c607274badead6f8ea6a263d8bd3b97932f
gaiaflati <gaiaflati@live.it>
2024-07-02 19:28:17 +0200

    agg

---
+++ TerzaPagina2/prova.py
@@ -0,0 +1,4 @@
+for i in range(5):
+    print('<div class="container text-center">')
+    print(' <div class="row" style="padding: 5%;">')
+    lancia programma con nome file>fil dove printare le cose
