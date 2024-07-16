a1fa9f97d60bcc9306d62445a516d6b5ebe248c1
gaiaflati <gaiaflati@live.it>
2024-07-16 19:23:43 +0200

    commit

--- TerzaPagina2/prova.py
+++
@@ -1,4 +0,0 @@
-for i in range(5):
-    print('<div class="container text-center">')
-    print(' <div class="row" style="padding: 5%;">')
-    lancia programma con nome file>fil dove printare le cose
